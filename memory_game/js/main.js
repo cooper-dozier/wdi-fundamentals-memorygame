98348953fd1afdb0cdcaebf4168605d8b4457c68
Cooper <cooper.dozier@gmail.com>
2019-09-12 13:39:47 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -50,27 +50,47 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry! Try again.");
+
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry! Try again.");
+		}
 	}
 }
 
-function flipCard(cardID) {
+function flipCard() {
+	var cardID = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardID].cardImage);
 	console.log("User flipped " + cards[cardID].rank);
 	cardsInPlay.push(cards[cardID]);
 	checkForMatch();
 }
 
+/*
+When the user clicks on a card, the image with the face of that card should be displayed. If the user has selected two cards, an alert should pop up letting the user know if the two cards match.
+*/
 
+function createBoard() {
+	for (var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
+createBoard();
+/*
 flipCard(0);
 console.log(cardsInPlay[0].suit);
 console.log(cardsInPlay[0].cardImage);
 flipCard(2);
 console.log(cardsInPlay[1].suit);
 console.log(cardsInPlay[1].cardImage);
+*/
 // checkForMatch();
 
 
